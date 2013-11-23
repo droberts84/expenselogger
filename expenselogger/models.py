c1824cdcfa3db2e2e8ae47ffaabb22f694fe758e
David Roberts <drob@drob.net>
2013-11-23 14:51:43 -0500

    Fixed issue for large expenses

--- expenselogger/models.py
+++ expenselogger/models.py
@@ -10,7 +10,7 @@ class Expense(models.Model):
 	# table attributes
 	name = models.CharField(max_length=100)
 	expense_type = models.CharField(max_length=1, choices=EXPENSE_TYPES)
-	amount = models.DecimalField(max_digits=5, decimal_places=2)
+	amount = models.DecimalField(max_digits=7, decimal_places=2)
 	date = models.DateField()
 
 	# string representation as expense name
