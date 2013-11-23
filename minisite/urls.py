7a51d99daca5d950509c0416b29f3630fca546f1
David Roberts <drob@drob.net>
2013-11-22 22:08:54 -0500

    enabled admin view

--- minisite/urls.py
+++ minisite/urls.py
@@ -1,7 +1,12 @@
 from django.conf.urls import patterns, include, url
+from django.contrib import admin
+admin.autodiscover()
 
 from expenselogger import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
+    #implemented directly in project root to avoid using expenselogger namespace
+    url(r'^/create/$', views.create_expense, name='create'), 
+    url(r'^admin/', include(admin.site.urls)),
 )
