26db415af452f5bead5790708bfc7c6f467489c1
David Roberts <drob@drob.net>
2013-11-23 02:58:01 -0500

    Updated test cases

--- expenselogger/tests.py
+++ expenselogger/tests.py
@@ -5,6 +5,7 @@ from django.core.urlresolvers import reverse
 import datetime
 from django.utils import timezone
 from django.test import TestCase
+from decimal import *
 from expenselogger.models import Expense #import model
 
 
@@ -23,53 +24,49 @@ class ExpenseViewsTestCase(TestCase):
 		resp = self.client.post(reverse('create'), {'name': 'Flight to ORD',
 													'expense_type': 'F',
 													'amount': 123.45,
-													'date': timezone.now(),})
+													'date': '11/01/13',
+													})
 		self.assertEqual(resp.status_code, 302)
 
 		# check attributes are storec correctly
-		e = Expense.objects.get(name='Flight to ORD')
-		self.assertEqual(e.amount, 123.45)
+		e = Expense.objects.get(name ='Flight to ORD')
+		self.assertAlmostEqual(e.amount, Decimal(123.45))
 		self.assertEqual(e.expense_type, 'F')
 
-		# still on index page
-		self.assertTrue('Expense Logger' in resp.content)
 
 
 	"""
-	Test validations on expnese submissions
+	Test validations on expense submissions
 	- Assuming all fields are required
-	- Further validations should checked for blanks, etc
+	- Further validations should be checked for blanks, etc
 	"""
 	def test_bad_expense_submissions(self):
-         # Send no POST data.
-		resp = self.client.post(reverse('create'))
-		self.assertEqual(resp.status_code, 200)
-		self.assertEqual(resp.context['error_message'], "Please specify a name for the expense.")
+
 
 		# Send no name
 		resp = self.client.post(reverse('create'), {'expense_type': 'F',
 													'amount': 123.45,
-													'date': timezone.now(),})
+													'date': '11/15/13',})
 		self.assertEqual(resp.status_code, 200)
-		self.assertEqual(resp.context['error_message'], "Please specify a name for the expense.")
+		self.assertEqual(resp.context['form'].errors['name'], [u'This field is required.'])
 
 		# No expense type specified
 		resp = self.client.post(reverse('create'), {'name': 'Flight to ORD',
 													'amount': 123.45,
-													'date': timezone.now(),})
+													'date': '11/15/13',})
 		self.assertEqual(resp.status_code, 200)
-		self.assertEqual(resp.context['error_message'], "Please specify a an expense type.")
+		self.assertEqual(resp.context['form'].errors['expense_type'], [u'This field is required.'])
 
 		# No amount specified
 		resp = self.client.post(reverse('create'), {'name': 'Flight to ORD',
 													'expense_type': 'F',
-													'date': timezone.now(),})
+													'date': '11/15/13',})
 		self.assertEqual(resp.status_code, 200)
-		self.assertEqual(resp.context['error_message'], "Please specify an amount for your expense")
+		self.assertEqual(resp.context['form'].errors['amount'], [u'This field is required.'])
 
-		# No amount specified
+		# No date specified
 		resp = self.client.post(reverse('create'), {'name': 'Flight to ORD',
 													'expense_type': 'F',
 													'amount': 123.45})
 		self.assertEqual(resp.status_code, 200)
-		self.assertEqual(resp.context['error_message'], "Please specify an amount for your expense")
+		self.assertEqual(resp.context['form'].errors['date'], [u'This field is required.'])
