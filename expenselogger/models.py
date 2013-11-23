3860c1231494a20ad1b5edbc28a2f76ff7a29fab
David Roberts <drob@drob.net>
2013-11-22 20:41:45 -0500

    created initial model

--- expenselogger/models.py
+++ expenselogger/models.py
@@ -1,4 +1,15 @@
 from django.db import models
 
 class Expense(models.Model):
-    name = models.CharField(max_length=100)
+	# choices for expense type fields
+	EXPENSE_TYPES = ( 
+		('F','Flight'),
+		('C','Car Rental'),
+		('H','Hotel'), )
+
+	# table attributes
+	name = models.CharField(max_length=100)
+	expense_type = models.CharField(max_length=1, choices=EXPENSE_TYPES)
+	amount = models.DecimalField(max_digits=5, decimal_places=2)
+	date = models.DateField()
+
