5d492fadb206bb623330cb8637b51f094f494b48
David Roberts <drob@drob.net>
2013-11-24 10:32:31 -0500

    Removed jsonify library and switched to serializing JSON in view to prevent deprecated warning, PASS

--- expenselogger/views.py
+++ expenselogger/views.py
@@ -2,12 +2,25 @@ from expenselogger.models import Expense
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from expenselogger.forms import ExpenseForm
+from django.core import serializers
 
 
 def index(request):
     expense_list = Expense.objects.all().order_by('-date')
     form = ExpenseForm()
-    context = {'expense_list': expense_list, 'form': form}
+    
+    """
+    serialize ALL expenses JSON to support submitting similar expenses in javascript
+    
+    All expenses are converted to prevent live remote call to get single expense
+    from database upon user click event.
+    If volume of expenses is high, this should be revaluated
+    """
+    json_expenses = serializers.serialize('json', expense_list);
+
+    
+
+    context = {'expense_list': expense_list, 'form': form, 'json_expenses': json_expenses}
     
     return render(request, 'expenselogger/index.html', context)
     
